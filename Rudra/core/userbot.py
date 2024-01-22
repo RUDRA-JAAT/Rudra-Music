1382d411a64d2dbb9b8197e4340c2d6039498b1c
RUDRA JAAT <136905244+RUDRA-JAAT@users.noreply.github.com>
2024-01-22 08:49:44 +0530

    Update userbot.py

--- Rudra/core/userbot.py
+++ Rudra/core/userbot.py
@@ -96,9 +96,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("RUDRA_JAAT_1")
+                await self.three.join_chat("RUDRA_JAAT_BIO")
                 
-                await self.one.join_chat("RUDRA_JAAT_1")
+                await self.one.join_chat("RUDRA_JAAT_BIO")
             except:
                 pass
             assistants.append(3)
@@ -118,9 +118,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("RUDRA_JAAT_1")
+                await self.four.join_chat("RUDRA_JAAT_BIO")
                 
-                await self.one.join_chat("RUDRA_JAAT_1")
+                await self.one.join_chat("RUDRA_JAAT_BIO")
             except:
                 pass
             assistants.append(4)
@@ -140,9 +140,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("RUDRA_JAAT_1")
+                await self.five.join_chat("RUDRA_JAAT_BIO")
                 
-                await self.one.join_chat("RUDRA_JAAT_1")
+                await self.one.join_chat("RUDRA_JAAT_BIO")
             except:
                 pass
             assistants.append(5)
