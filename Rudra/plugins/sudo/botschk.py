4f26fcb76a3be7b86cbcc2f8884fb0560c7a341d
RUDRA JAAT <136905244+RUDRA-JAAT@users.noreply.github.com>
2024-01-22 08:39:57 +0530

    Update botschk.py.

--- Rudra/plugins/sudo/botschk.py
+++ Rudra/plugins/sudo/botschk.py
@@ -1,6 +1,18 @@
+from Rudra.misc import db
+from Rudra.utils.database import get_assistant, get_authuser_names, get_cmode
+
 import asyncio
+import config
+import random
+import asyncio
+from datetime import datetime
+from pyrogram.enums import ChatType
+from Rudra.core.call import Rudra
+from Rudra.logging import LOGGER
+from config import *
 from pyrogram import filters
 from Rudra import app 
+from Rudra import app, userbot
 from Rudra.core.userbot import Client
 from Rudra.misc import SUDOERS
 
